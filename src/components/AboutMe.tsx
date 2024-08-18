3c73d6686f9466b82da3c2efa187c9d7fa223bc8
chloenevas <113219111+chloenevas@users.noreply.github.com>
2024-08-18 17:38:58 -0400

    added draft of about; working on animation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,33 +1,45 @@
 import React from "react";
+
 import "../styles/AboutMe.css";
 
 import photo from "../assets/photo.jpg"
 
 function AboutMe() {
+
+  document.body.classList.add("no-scroll");
+
   return (
     <div>
-      <div id="title"> Cutesy Little Title </div>
-      <div className="container">
-            <div className="text upper-left">
-              Here I'll write about where I grew up and ill say a few things
-              about it not too much just a little like prob this much
-            </div>
-            <div className="text lower-left">
-              Here I'll talk about school and what im doing there and how it's
-              all going. Ill prob talk about TAing and how much I love teaching.
-              will prob talk about GWC as well
-            </div>
-          <img id="photo" src={photo} alt="Photo of Chloe" />
-            <div className=" text upper-right">
-              Here I'll talk about my hobbies and interests! Things such as...
-              violin, music, baking, reading, pottery!, running
-            </div>
-            <div className="text lower-right">
-              guess i gotta figure out something else to talk about in this
-              section here
-            </div>
+      <div id="title"> About Me </div>
+      <div className="about-container">
+        <img id="photo" src={photo} alt="Photo of Chloe" />
+
+        <div className="circles">
+          <div className="text" id="blurb-1">
+            I'm from Westport, CT <br />
+            and I'm currently a junior at Brown University studying Applied Math-Computer Science.
+          </div>
+          <div className="text" id="blurb-2">
+            In my free time, I love <br />
+            baking, playing the violin, reading, running, and I recently just
+            started <br />
+            learning how to make pottery!
+          </div>
+          <div className=" text" id="blurb-3">
+            <br /> <br />
+            I absolutely love teaching
+            <br /> and helping others learn. At Brown, <br />
+            I've been an undergraduate teaching <br />
+            assistant for multiple introductory <br />
+            CS classes, and I'm currently a head <br />
+            teaching assistant for Object Oriented Programming. I was also lucky
+            enough to <br />
+            spend this past summer as a lead teaching assistant for Girls Who
+            Code.
+          </div>
         </div>
       </div>
+    </div>
   );
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import "../styles/Home.css";
+import linkedin from "../assets/linkedin.png"
+import mail from "../assets/mail.png";
 
 function Home() {
   return (
-    <div id="Intro-text">
-        Hi, I'm Chloe!
+    <div className="home-container">
+      <div className="dark-rectangle"></div>
+      <div className="light-rectangle"></div>
+      <div id="hi-text">Hi,</div>
+      <div id="name-text">I'm Chloe!</div>
+      <div id="description">programmer, student, teacher</div>
+      <div className="contact-container">
+        <a
+          className="contact"
+          href="https://www.linkedin.com/in/chloe-nevas/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          <img className="photo" src={linkedin} alt="Linkedin logo" />
+          <div className="contact-item">Chloe Nevas</div>
+        </a>
+        <a
+          className="contact"
+          href="mailto:chloe_nevas@brown.edu"
+          style={{ textDecoration: "none" }}
+        >
+          <img className="photo" src={mail} alt="Linkedin logo" />
+          <div className="contact-item">chloe_nevas@brown.edu</div>
+        </a>
+        <div></div>
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
+      </div>
     </div>
   );
 }
